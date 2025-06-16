b007de6a1e82e80446d5d12961542c93dc7cccec
Shubham Karvariya <shubhamkarwariya245@gmail.com>
2025-06-16 18:06:47 +0530

    Time: 1583 ms (7.43%) | Memory: 310 MB (10.32%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,13 +1,30 @@
-from functools import lru_cache
+# from functools import lru_cache
+# class Solution:
+#     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+#         @lru_cache(None)
+#         def backtrack(i1, i2):
+#             if i1==len(text1) or i2==len(text2):
+#                 return 0
+#             if text1[i1]==text2[i2]:
+#                 return 1+backtrack(i1+1, i2+1)
+#             else:
+#                 return max(backtrack(i1, i2+1), backtrack(i1+1, i2))
+                
+#         return backtrack(0,0)
+
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        @lru_cache(None)
-        def backtrack(i1, i2):
-            if i1==len(text1) or i2==len(text2):
+        def backtrack(i, j):
+            if i==len(text1) or j==len(text2):
                 return 0
-            if text1[i1]==text2[i2]:
-                return 1+backtrack(i1+1, i2+1)
+            if (i, j) in memo:
+                return memo[(i,j)]
+            if text1[i]==text2[j]:
+                memo[(i,j)] = 1 + backtrack(i+1, j+1)
             else:
-                return max(backtrack(i1, i2+1), backtrack(i1+1, i2))
-                
+                memo[(i,j)] =  max(backtrack(i+1,j), backtrack(i,j+1))
+            
+            return memo[(i,j)]
+        
+        memo={}
         return backtrack(0,0)
