43631ffa0b85b5a7654b7d9e3d6953aa8fcd511f
Shubham Karvariya <shubhamkarwariya245@gmail.com>
2025-06-17 18:17:44 +0530

    Time: 16 ms (5.85%) | Memory: 21.2 MB (10.55%) - LeetSync

---
+++ 120-triangle/triangle.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+
+        def dp(row, col):
+            if row==len(triangle):
+                return 0
+            if (row, col) in memo:
+                return memo[(row, col)]
+            left = dp(row+1, col)
+            right = dp(row+1, col+1)
+
+            memo[(row, col)] = triangle[row][col] + min(left, right)
+            return memo[(row, col)]
+
+        if len(triangle)==1:
+            return triangle[0][0]
+        memo = {}
+        return dp(0, 0)
+        
