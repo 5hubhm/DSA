a643201adae4c1525c4d194e6de677e427cb711e
Shubham Karvariya <shubhamkarwariya245@gmail.com>
2025-06-17 18:19:08 +0530

    Time: 7 ms (36.25%) | Memory: 21.3 MB (8.61%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -1,19 +1,36 @@
+# class Solution:
+#     def minimumTotal(self, triangle: List[List[int]]) -> int:
+
+#         def dp(row, col):
+#             if row==len(triangle):
+#                 return 0
+#             if (row, col) in memo:
+#                 return memo[(row, col)]
+#             left = dp(row+1, col)
+#             right = dp(row+1, col+1)
+
+#             memo[(row, col)] = triangle[row][col] + min(left, right)
+#             return memo[(row, col)]
+
+#         if len(triangle)==1:
+#             return triangle[0][0]
+#         memo = {}
+#         return dp(0, 0)
+
+from functools import lru_cache
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
 
+        @lru_cache(None)
         def dp(row, col):
             if row==len(triangle):
                 return 0
-            if (row, col) in memo:
-                return memo[(row, col)]
             left = dp(row+1, col)
             right = dp(row+1, col+1)
 
-            memo[(row, col)] = triangle[row][col] + min(left, right)
-            return memo[(row, col)]
+            return triangle[row][col] + min(left, right)
 
         if len(triangle)==1:
             return triangle[0][0]
-        memo = {}
         return dp(0, 0)
         
